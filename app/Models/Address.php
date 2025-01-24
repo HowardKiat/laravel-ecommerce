acd0c1358673c9c765668a478c740b848f37d0bf
HowardKiat <buisnesskiat123@gmail.com>
2025-01-24 10:14:12 +0800

    Importing models

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
 {
-    //
+    use HasFactory;
+    
+    protected $fillable = [
+        'order_id',
+        'firs_name',
+        'last_name',
+        'email',
+        'phone',
+        'street_address',
+        'city',
+        'state',
+        'zip_code',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function getFullNameAttribute()
+    {
+        return "{$this->first_name} {$this->last_name}";
+    }
 }
